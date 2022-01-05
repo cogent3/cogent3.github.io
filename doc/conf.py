b1b01809cb88c719345178f28b84af4dfdc63f16
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2022-01-05 16:32:34 +1100

    DOC: refactor to use nbsphinx for making the gallery

--- doc/conf.py
+++ doc/conf.py
@@ -5,6 +5,16 @@ import shutil
 import sys
 from glob import glob
 
+
+def make_nbsphinx_thumbnails():
+    """returns dict of {path: '_images/{path.stem}'"""
+    gallery = [
+        p for p in pathlib.Path("doc/draw").glob("**/*.rst") if p.stem != "README"
+    ]
+
+    return {str(n).split(".")[0]: f"_images/{n.stem}.png" for n in gallery}
+
+
 # sphinx_navtree
 today = datetime.date.today()
 year = today.strftime("%Y")
@@ -41,6 +51,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
     "sphinxcontrib.bibtex",
+    "sphinx_gallery.load_style",
     "sphinx_panels",
 ]
 
@@ -85,9 +96,6 @@ exclude_patterns = [
     ".DS_Store",
     "**.ipynb_checkpoints",
     "cookbook/union_dict.rst",
-    "dev/draw_examples/README.rst",
-    "dev/draw_examples/aln/README.rst",
-    "dev/draw_examples/tree/README.rst",
 ]
 
 # The encoding of source files.
@@ -109,64 +117,13 @@ html_static_path = ["_static"]
 
 htmlhelp_basename = "cogent3doc"
 
-# -- Options for Sphinx Gallery
+# -- Options for Gallery
 
 nbsphinx_requirejs_path = "require.js"
+nbsphinx_thumbnails = make_nbsphinx_thumbnails()
 
 
-def plotly_sg_scraper(block, block_vars, gallery_conf, *args, **kwargs):
-    examples_dirs = gallery_conf["examples_dirs"]
-    if isinstance(examples_dirs, (list, tuple)):
-        examples_dirs = examples_dirs[0]
-    pngs = sorted(glob(os.path.join(examples_dirs, "**", "*.png"), recursive=True))
-    htmls = sorted(glob(os.path.join(examples_dirs, "**", "*.html"), recursive=True))
-    image_path_iterator = block_vars["image_path_iterator"]
-    image_names = list()
-    seen = set()
-    for html, png in zip(htmls, pngs):
-        if png not in seen:
-            seen |= {png}
-            this_image_path_png = next(image_path_iterator)
-            this_image_path_html = os.path.splitext(this_image_path_png)[0] + ".html"
-            image_names.append(this_image_path_html)
-            shutil.move(png, this_image_path_png)
-            shutil.move(html, this_image_path_html)
-
-    # Use the `figure_rst` helper function to generate rST for image files
-    from plotly.io._sg_scraper import figure_rst
-
-    return figure_rst(image_names, gallery_conf["src_dir"])
-
-
-def plotly_sg_scraper_nb(*args, **kwargs):
-    try:
-        result = plotly_sg_scraper(*args, **kwargs)
-    except Exception:
-        result = ""
-    return result
-
-
-c3_doc_dir = pathlib.Path("doc")
-if c3_doc_dir.exists():
-    from sphinx_gallery.sorting import ExplicitOrder, FileNameSortKey
-
-    templates_path = ["doc/templates"]
-    image_scrapers = (plotly_sg_scraper_nb,)
-    extensions.append("sphinx_gallery.gen_gallery")
-    example_dirs = ["doc/draw_examples"]
-    gallery_dirs = ["doc/draw"]
-
-    sphinx_gallery_conf = {
-        "examples_dirs": example_dirs,
-        "subsection_order": ExplicitOrder(
-            ["doc/draw_examples/aln", "doc/draw_examples/tree"]
-        ),
-        "abort_on_example_error": True,
-        "within_subsection_order": FileNameSortKey,
-        "gallery_dirs": gallery_dirs,
-        "image_scrapers": image_scrapers,
-        "download_all_examples": False,
-    }
+templates_path = ["doc/templates"]
 
 # -- Options for LaTeX output --------------------------------------------------
 latex_documents = [
