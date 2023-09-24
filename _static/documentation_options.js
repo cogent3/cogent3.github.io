b7280018e6b68a1d4135599bfec5ad64bf674bd5
GavinHuttley <GavinHuttley@users.noreply.github.com>
2023-09-24 23:20:22 +0000

    deploy: 3a61024558a79cd4f1a9a32b50c7417b36fbe6ec

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,7 +1,6 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2023.09.23',
-    LANGUAGE: 'None',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '2023.09.24',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
